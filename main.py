7984ba3258b6ff12f0f97329051bfeb99dfdc22f
Bernhard Stern <bernhard.stern@keemail.me>
2021-10-23 15:23:34 +0200

    Changed main.py and started a snake.py (Class) - creates snake in a class instead in main.py

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
+import time
 from turtle import Screen, Turtle
+from snake import Snake
+from time import sleep
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
-# Create a initial Snake with square-turtles
-square_list = []
-for initial_square in range(0, 3):
-    new_square = Turtle()
-    new_square.shape("square")
-    new_square.color("white")
-    new_square.penup()
-    new_square.setx(0 - 20*initial_square)
+# TODO Create a initial Snake with square-turtles
+
+snake = Snake()
 
 # TODO Move the Snake
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+
 # TODO Steer the Snake
 # TODO Create Food as collision object on the road
 # TODO Expand the Snake if it collides with food
---
+++ snake.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+# Constant convention: BIG_LETTERS_ONLY
+# Helps to change things fast, without searching the code
+
+MOVE_DISTANCE = 20
+
+class Snake():
+    def __init__(self):
+        self.body = []
+        self.new_body()
+
+    def new_body(self):
+        for initial_square in range(0, 3):
+            new_square = Turtle()
+            new_square.shape("square")
+            new_square.color("white")
+            new_square.penup()
+            new_square.setx(0 - 20 * initial_square)
+            self.body.append(new_square)
+
+    def move(self):
+        for tile_num in range(len(self.body) - 1, 0, -1):
+            new_x = self.body[tile_num - 1].xcor()
+            new_y = self.body[tile_num - 1].ycor()
+            self.body[tile_num].goto(new_x, new_y)
+        self.body[0].forward(MOVE_DISTANCE)
